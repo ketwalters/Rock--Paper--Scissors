e6577cfda97953124b3a7d2b2c4791cf4b0faa09
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2013-12-10 13:45:19 -0500

    require

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ require './hand.rb'
 module Game
   class RPS_App < Sinatra::Application
 	get '/throw/:type' do
-	   	@random = Hand.new(params[:type])
+	   	@rpsl = Hand.new(params[:type])
 	    erb :hand
 	end
 
